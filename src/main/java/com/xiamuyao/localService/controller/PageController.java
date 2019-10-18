0c29e9388ce5a28ccdd007128c893efc542a57fd
XiaMuYao <XiaMuYaodqx@gmail.com>
2019-10-18 23:41:36 +0800

    修改jar包目录位当前同级目录 并且发布jar包

--- src/main/java/com/xiamuyao/localService/controller/PageController.java
+++ src/main/java/com/xiamuyao/localService/controller/PageController.java
@@ -2,6 +2,9 @@ package com.xiamuyao.localService.controller;
 
 import org.apache.commons.io.Charsets;
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.slf4j.SLF4JLogger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,84 +15,88 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.util.LoggingUtils;
 
 import java.io.File;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
 public class PageController {
 
-	private static final Charset CHARSET = Charsets.toCharset("UTF-8");
+    private static final Charset CHARSET = Charsets.toCharset("UTF-8");
 
-	@Value("${app.stub.folder}")
-	private String stubFolder;
+    @Value("${app.stub.folder}")
+    private String stubFolder;
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(PageController.class);
 
-	@GetMapping("/")
-	public String index(ModelMap map, @ModelAttribute("type") String type) {
-		List<String> existsFiles = new ArrayList<>();
-		if (!StringUtils.isEmpty(stubFolder)) {
-			File folder = new File(stubFolder);
-			File[] listOfFiles = folder.listFiles();
-			for (int i = 0; i < listOfFiles.length; i++) {
-				if (listOfFiles[i].isFile()) {
-					existsFiles.add(listOfFiles[i].getName());
-				}
-			}
-		}
-		if ("upload-error".equals(type)) {
-			map.addAttribute("message", " Please select json file.");
-		}
-		else if ("upload-ok".equals(type)) {
-			map.addAttribute("message", " Upload ok.");
-		}
-		else if ("update-ok".equals(type)) {
-			map.addAttribute("message", " Update ok.");
-		}
-		map.addAttribute("fileList", existsFiles);
-		return "index";
-	}
 
-	@GetMapping("/detail")
-	public String detail(ModelMap map, @RequestParam("f") String f) throws Exception {
-		if (StringUtils.isEmpty(f)) {
-			return null;
-		}
-		File file = new File(stubFolder+ File.separator + f);
-		String fileContent = FileUtils.readFileToString(file, CHARSET);
-		map.addAttribute("fileName", f);
-		map.addAttribute("fileContent", fileContent);
-		return "detail";
-	}
+    @GetMapping("/")
+    public String index(ModelMap map, @ModelAttribute("type") String type) {
+        List<String> existsFiles = new ArrayList<>();
+        if (!StringUtils.isEmpty(stubFolder)) {
+            File folder = new File(stubFolder);
+            String name = folder.getAbsolutePath();
+			logger.debug("当前路径::"+name);
+            File[] listOfFiles = folder.listFiles();
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    existsFiles.add(listOfFiles[i].getName());
+                }
+            }
+        }
+        if ("upload-error".equals(type)) {
+            map.addAttribute("message", " Please select json file.");
+        } else if ("upload-ok".equals(type)) {
+            map.addAttribute("message", " Upload ok.");
+        } else if ("update-ok".equals(type)) {
+            map.addAttribute("message", " Update ok.");
+        }
+        map.addAttribute("fileList", existsFiles);
+        return "index";
+    }
 
-	@PostMapping("/update")
-	public String update(ModelMap map, 
-			@RequestParam("fileName") String fileName, 
-			@RequestParam("fileContent") String fileContent,
-			RedirectAttributes redirectAttributes) throws Exception {
+    @GetMapping("/detail")
+    public String detail(ModelMap map, @RequestParam("f") String f) throws Exception {
+        if (StringUtils.isEmpty(f)) {
+            return null;
+        }
+        File file = new File(stubFolder + File.separator + f);
+        String fileContent = FileUtils.readFileToString(file, CHARSET);
+        map.addAttribute("fileName", f);
+        map.addAttribute("fileContent", fileContent);
+        return "detail";
+    }
 
-		File file = new File(stubFolder+ File.separator + fileName);
-		if (!file.exists()) {
-			redirectAttributes.addFlashAttribute("type", "update-error");
-			return "redirect:/";
-		}
-		FileUtils.write(file, fileContent, CHARSET);
-		redirectAttributes.addFlashAttribute("type", "update-ok");
-		return "redirect:/";
-	}
+    @PostMapping("/update")
+    public String update(ModelMap map,
+                         @RequestParam("fileName") String fileName,
+                         @RequestParam("fileContent") String fileContent,
+                         RedirectAttributes redirectAttributes) throws Exception {
 
-	@PostMapping("/upload")
-	public String upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)  throws Exception {
-		String filename = file.getOriginalFilename();
-		if (!filename.endsWith("json")) {
-			redirectAttributes.addFlashAttribute("type", "upload-error");
-			return "redirect:/";
-		}
-		File newfile = new File(stubFolder+ File.separator + file.getOriginalFilename());  
-		file.transferTo(newfile); 
-		redirectAttributes.addFlashAttribute("type", "upload-ok");
-		return "redirect:/";
-	}
+        File file = new File(stubFolder + File.separator + fileName);
+        if (!file.exists()) {
+            redirectAttributes.addFlashAttribute("type", "update-error");
+            return "redirect:/";
+        }
+        FileUtils.write(file, fileContent, CHARSET);
+        redirectAttributes.addFlashAttribute("type", "update-ok");
+        return "redirect:/";
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws Exception {
+        String filename = file.getOriginalFilename();
+        if (!filename.endsWith("json")) {
+            redirectAttributes.addFlashAttribute("type", "upload-error");
+            return "redirect:/";
+        }
+        File newfile = new File(stubFolder + File.separator + file.getOriginalFilename());
+        file.transferTo(newfile);
+        redirectAttributes.addFlashAttribute("type", "upload-ok");
+        return "redirect:/";
+    }
 
 }
